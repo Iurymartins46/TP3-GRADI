7148c711314f699673fe776e49e8b139f00d14d5
unknown <afbastos8@gmail.com>
2023-11-28 22:48:50 -0300

    :guitar: feat: Adicionar funões de pesquisa e inserção

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ api = ManipularApiTmdb()
 servico = Servicos()
 
 print("-------------------------------------------------")
-boolean = dataBase.criarDataBase()
+#boolean = dataBase.criarDataBase()
 #dados = api.obter_informacoes_filme_por_id(565770)
 #teste = dataBase.inserirFilmeDataBase(dados_filme=dados)
 #print(dataBase.recuperarFilmePorID(1))
-
 #servico.popularDataBase()
-print(dataBase.recuperarIndiceInvertido())
+#print(dataBase.recuperarIndiceInvertido())
 print("-------------------------------------------------")
--- backend/apiFront.py
+++ backend/apiFront.py
@@ -40,14 +40,47 @@ def realizarLogin():
 
     return data, 200
 
-"""
-@app.route('/listarDiariosPorTag', methods=['GET'])
-def listarDiariosPorTag():
-    manipularDataBase = ManipularDataBase()
-    usuario_id = request.json.get("usuario_id")
-    tag = request.json.get("tag")
-    recebelista = manipularDataBase.listarDiariosPorTag(usuario_id=usuario_id,tag=tag)
+
+@app.route('/pesquisarFilme', methods=['GET'])
+def pesquisarFilme():
+    string = request.json.get("string")
+    recebelista = Servicos.pesquisarFilme(string=string)
     return recebelista, 200
-"""
+
+@app.route('/adicionarFilme', methods=['POST'])
+def adicionarFilme():
+    id = request.json.get("id")
+    titulo = request.json.get("titulo")
+    generos = request.json.get("generos")
+    orcamento = request.json.get("orcamento")
+    receita = request.json.get("receita")
+    data_lancamento = request.json.get("data_lancamento")
+    tempo_duracao = request.json.get("tempo_duracao")
+    slogan = request.json.get("slogan")
+    popularidade = request.json.get("popularidade")
+    total_votos = request.json.get("total_votos")
+    media_votos = request.json.get("media_votos")
+    poster_path = request.json.get("poster_path")
+    sinopse = request.json.get("sinopse")
+
+    dicionario = dict() 
+    dicionario ["id"] = id
+    dicionario ["titulo"]= titulo
+    dicionario ["generos"] = {i: valor for i, valor in enumerate(generos)}
+    dicionario ["orcamento"]= orcamento
+    dicionario ["receita"]= receita
+    dicionario ["data_lancamento"]= data_lancamento
+    dicionario ["tempo_duracao"]= tempo_duracao
+    dicionario ["slogan"]= slogan
+    dicionario ["popularidade"]= popularidade
+    dicionario ["total_votos"]= total_votos
+    dicionario ["media_votos"]= media_votos
+    dicionario ["poster_path"]= poster_path
+    dicionario ["sinopse"]= sinopse
+    
+    insereFilme = Servicos.adicionarFilme(dicionario)
+    
+    return insereFilme, 200
+
 if __name__ == '__main__':
     app.run(debug=True)
